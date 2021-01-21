10f92fd2607ff85a10dc1d08fc89fbc6e3606670
Xparham <xparham@gmail.com>
2021-01-21 14:34:31 -0500

    corrected syntax errors

--- results.js
+++ results.js
@@ -1,11 +1,26 @@
-const searchBar = document.querySelector("#search-bar");
-const searchBtn = document.querySelector("#search-button");
+const jobType = document.querySelector("#job-type");
+// const searchBtn = document.querySelector("#search-button");
 const resultsSection = document.querySelector("#results");
 const searchForm = document.querySelector("#search-form");
 
-searchBtn,addEventListener('search-button', function(){
+searchForm.addEventListener("submit", function(e){
+    e.preventDefault();
+   
+    const resultsSection = searchJobs(jobType.value)
+    console.log(resultsSection)
+
+
+
 
 });
 
+function searchJobs(jobString){
+    return{
+        jobName : "Javascript Developer",
+        JobSalary : '60_000'
+
+    }
+
+}
 let jobs = [];
 
