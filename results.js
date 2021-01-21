763030180bd8d6ca0524fbd31e7d892508d1dd69
Xparham <xparham@gmail.com>
2021-01-21 14:09:13 -0500

    made function for seach from

--- results.js
+++ results.js
@@ -1,9 +1,11 @@
-const searchBar = document.querySelector("#search-bar")
-const searchBtn = document.querySelector("#search-button")
-const resultsSection = document.querySelector("#results")
-const searchForm = document.querySelector("#search-form")
+const searchBar = document.querySelector("#search-bar");
+const searchBtn = document.querySelector("#search-button");
+const resultsSection = document.querySelector("#results");
+const searchForm = document.querySelector("#search-form");
 
-searchBtn,addEventListener('search-button')
+searchBtn,addEventListener('search-button', function(){
+
+});
 
 let jobs = [];
 
