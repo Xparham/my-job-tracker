e16d480887051fe627c72301700a454c02c4fab6
Xparham <xparham@gmail.com>
2021-01-21 16:42:02 -0500

    Added API url

--- results.js
+++ results.js
@@ -3,38 +3,40 @@ const jobType = document.querySelector("#job-type");
 const resultsSection = document.querySelector("#results");
 const searchForm = document.querySelector("#search-form");
 
+const url = 'http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=19d80290&app_key=4b2076ceb89c4ffbcf6b0e543402fa9f&results_per_page=10&what=javascript%20developer&content-type=application/json'
+
 let sampleData = {
-    "salary_min": 50000,
-    "longitude": -0.776902,
-    "location": {
-      "__CLASS__": "Adzuna::API::Response::Location",
-      "area": [
-        "UK",
-        "South East England",
-        "Buckinghamshire",
-        "Marlow"
-      ],
-      "display_name": "Marlow, Buckinghamshire"
-    },
-    "salary_is_predicted": 0,
-    "description": "JavaScript Developer Corporate lorfdfgdgrtgrtgrgbgbdfgbgnbjkrkbntrkjhbwhtgwltrblwutigh",
-    "__CLASS__": "Adzuna::API::Response::Job",
-    "created": "2013-11-08T18:07:39Z",
-    "latitude": 51.571999,
-    "redirect_url": "http://adzuna.co.uk/jobs/land/ad/129698749...",
-    "title": "Javascript Developer",
-    "category": {
-      "__CLASS__": "Adzuna::API::Response::Category",
-      "label": "IT Jobs",
-      "tag": "it-jobs"
-    },
-    "id": "129698749",
-    "salary_max": 55000,
-    "company": {
-      "__CLASS__": "Adzuna::API::Response::Company",
-      "display_name": "Corporate Project Solutions"
-    },
-}
+    // "salary_min": 50000,
+    // "longitude": -0.776902,
+    // "location": {
+    //   "__CLASS__": "Adzuna::API::Response::Location",
+    //   "area": [
+    //     "UK",
+    //     "South East England",
+    //     "Buckinghamshire",
+    //     "Marlow"
+    //   ],
+    //   "display_name": "Marlow, Buckinghamshire"
+    // },
+    // "salary_is_predicted": 0,
+    // "description": `${jobString} Corporate lorfdfgdgrtgrtgrgbgbdfgbgnbjkrkbntrkjhbwhtgwltrblwutigh`,
+    // "__CLASS__": "Adzuna::API::Response::Job",
+    // "created": "2013-11-08T18:07:39Z",
+    // "latitude": 51.571999,
+    // "redirect_url": "http://adzuna.co.uk/jobs/land/ad/129698749...",
+    // "title": `${jobString}`,
+    // "category": {
+    //   "__CLASS__": "Adzuna::API::Response::Category",
+    //   "label": "IT Jobs",
+    //   "tag": "it-jobs"
+    // },
+    // "id": "129698749",
+    // "salary_max": 55000,
+    // "company": {
+    //   "__CLASS__": "Adzuna::API::Response::Company",
+    //   "display_name": "Corporate Project Solutions"
+    // },
+};
 
 let jobs = []
 
@@ -45,28 +47,51 @@ searchForm.addEventListener("submit", function(e){
 
     resultsSection.innerHTML = '<div id="results"></div>'
 
-    jobs.forEach(function(job){
-        console.log(job.title)
-        console.log(job.description)
+    jobs.forEach(function (job){
 
         const div =document.createElement('div');
-        div.innerHTML=`<h4 class="p-6 mx-6">${job.title}</h4>  <p class="p-6 mx-6">${job.description}</p>`;
+        div.innerHTML=`<h4 class="p-6 mx-6 font-bold text-3xl">${job.title}</h4>  <p class="p-6 mx-6">${job.description}</p>`;
         resultsSection.appendChild(div)
 
     });
 
-
-
-
 });
 
 function searchJobs(jobString) {
-    jobs.push(sampleData)
-    return sampleData
-
-        
-          
-
+    jobs.push({
+        "salary_min": 50000,
+        "longitude": -0.776902,
+        "location": {
+          "__CLASS__": "Adzuna::API::Response::Location",
+          "area": [
+            "UK",
+            "South East England",
+            "Buckinghamshire",
+            "Marlow"
+          ],
+          "display_name": "Marlow, Buckinghamshire"
+        },
+        "salary_is_predicted": 0,
+        "description": `${jobString} Corporate lorfdfgdgrtgrtgrgbgbdfgbgnbjkrkbntrkjhbwhtgwltrblwutigh`,
+        "__CLASS__": "Adzuna::API::Response::Job",
+        "created": "2013-11-08T18:07:39Z",
+        "latitude": 51.571999,
+        "redirect_url": "http://adzuna.co.uk/jobs/land/ad/129698749...",
+        "title": `${jobString}`,
+        "category": {
+          "__CLASS__": "Adzuna::API::Response::Category",
+          "label": "IT Jobs",
+          "tag": "it-jobs"
+        },
+        "id": "129698749",
+        "salary_max": 55000,
+        "company": {
+          "__CLASS__": "Adzuna::API::Response::Company",
+          "display_name": "Corporate Project Solutions"
+        }
+    });
+    
+    return sampleData;
 }
 
 
