9a731f18b30a69f5697d36f17ce176cbee36fb6b
Xparham <xparham@gmail.com>
2021-01-21 13:59:39 -0500

    Added selector for search form

--- results.js
+++ results.js
@@ -1,6 +1,7 @@
 const searchBar = document.querySelector("#search-bar")
 const searchBtn = document.querySelector("#search-button")
 const resultsSection = document.querySelector("#results")
+const searchForm = document.querySelector("#search-form")
 
 searchBtn,addEventListener('search-button')
 
