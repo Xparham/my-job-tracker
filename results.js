006ac11b01db5b4d617dc46fa8279b16d104b0b3
Xparham <xparham@gmail.com>
2021-01-21 13:56:21 -0500

    Added event listener for search button

--- results.js
+++ results.js
@@ -2,5 +2,7 @@ const searchBar = document.querySelector("#search-bar")
 const searchBtn = document.querySelector("#search-button")
 const resultsSection = document.querySelector("#results")
 
+searchBtn,addEventListener('search-button')
+
 let jobs = [];
 
