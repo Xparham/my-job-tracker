f348c7e1389ba182bb0b57a1cc632b87da150145
Xparham <xparham@gmail.com>
2021-01-21 13:50:39 -0500

    added array for jobs

--- results.js
+++ results.js
@@ -1,2 +1,6 @@
-const searchBar = document.querySelector('#search-bar')
-const searchBtn = document.querySelector('#search-button')
+const searchBar = document.querySelector("#search-bar")
+const searchBtn = document.querySelector("#search-button")
+const resultsSection = document.querySelector("#results")
+
+let jobs = [];
+
