3e2e033a3cb4f1853bea77bd62a7f54b06115773
Xparham <xparham@gmail.com>
2021-01-25 17:08:33 -0500

    added selectors from login button, login modal, register button, register modals"

---
+++ landing-page.js
@@ -0,0 +1,4 @@
+const loginBtn = document.querySelector("#login")
+const registerBtn = document.querySelector("#register")
+const loginModal = document.querySelector("#login-modal")
+const registerModal = document.querySelector("#register-modal")
