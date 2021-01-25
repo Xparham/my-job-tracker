a373166c347fa7767340f4090e5cb9296ec7f9ed
Xparham <xparham@gmail.com>
2021-01-25 17:20:33 -0500

    added event listeners for login and register buttons

--- landing-page.js
+++ landing-page.js
@@ -2,3 +2,13 @@ const loginBtn = document.querySelector("#login")
 const registerBtn = document.querySelector("#register")
 const loginModal = document.querySelector("#login-modal")
 const registerModal = document.querySelector("#register-modal")
+
+loginBtn.addEventListener('click', function(){
+    loginModal.classList.remove('hidden')
+
+
+})
+
+registerBtn.addEventListener('click', function(){
+    registerModal.classList.remove('hidden')
+})
