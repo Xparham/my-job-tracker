290c50a63a6e1c537ff9fcc917f16036975e6b7a
Xparham <xparham@gmail.com>
2021-01-29 11:00:57 -0500

    Added firebase configuration

---
+++ firebase-init.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBCjx8n1p6iS3Jvu8GiExli0ibYPg99-Gg",
+    authDomain: "serverless-auth-1fb7d.firebaseapp.com",
+    projectId: "serverless-auth-1fb7d",
+    storageBucket: "serverless-auth-1fb7d.appspot.com",
+    messagingSenderId: "977168107973",
+    appId: "1:977168107973:web:40ded3ca42586132c5e3ac"
+  };
